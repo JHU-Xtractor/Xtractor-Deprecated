0c9f409145deec062ad8b3df2070e69e894f7ed3
zatebe <mkorman3@jh.edu>
2023-04-30 17:56:23 -0400

    Change to multiple tabs working on the same window

--- XtractorGUI.py
+++ XtractorGUI.py
@@ -10,91 +10,99 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Create the central widget
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
-
         self.setWindowIcon(QIcon('resources/logo.png'))
         self.setWindowTitle("Xtractor")
         self.setGeometry(300, 300, 800, 600)
         self.setWindowState(Qt.WindowMaximized)
 
-        # Create the vertical layout for the central widget
-        vertical_layout = QHBoxLayout(central_widget)
+        # Create the central tab bar widget
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
 
-        # Create the tab widget
-        self.tab_widget = QTabWidget(self)
-        self.tab_widget.setTabPosition(QTabWidget.South)
+        # Create the vertical layout for the central widget
+        main_layout = QVBoxLayout(central_widget)
+        self.bar = QTabBar(central_widget)
+        self.window = QWidget(central_widget)
+        self.window.setObjectName("main")
+        self.window.setStyleSheet('QWidget#main{background-image:url("resources/background.png"); background-position: center; background-repeat: no-repeat;}')
+        self.bar.setShape(QTabBar.Shape.TriangularSouth)
+        vertical_layout = QHBoxLayout(self.window)
+        main_layout.addWidget(self.bar)
+        main_layout.addWidget(self.window)
 
         # Create empty dict of file_paths
         self.file_paths = {}
+        # Create empty dict for data
+        self.data = {}
 
         # Create the plus button to add new tabs
-        plus_button = QPushButton("+", self)
-        plus_button.clicked.connect(self.add_new_tab)
-
-        self.plus_tab = QWidget()
-        self.plus_tab.setObjectName("main")
-        # Set the background image
-        self.plus_tab.setStyleSheet('QWidget#main{background-image:url("resources/background.png"); background-position: center; background-repeat: no-repeat;}')
+        self.plus_button = QPushButton("+", self.bar)
+        self.plus_button.clicked.connect(self.add_new_tab)
 
         # Add the plus button to the tab widget
-        self.tab_widget.addTab(self.plus_tab, "")
-        self.tab_widget.setTabEnabled(self.tab_widget.count() - 1, False)
-        self.tab_widget.tabBar().setTabButton(self.tab_widget.count() - 1, QTabBar.RightSide, plus_button)
-        self.tab_widget.setTabsClosable(True)
-        self.tab_widget.tabCloseRequested.connect(lambda index: self.tab_widget.removeTab(index))
+        self.bar.addTab("")
+        self.bar.setTabEnabled(self.bar.count() - 1, False)
+        self.bar.setTabButton(self.bar.count() - 1, QTabBar.RightSide, self.plus_button)
+        self.bar.tabCloseRequested.connect(self.remove_tab)
+        self.bar.currentChanged.connect(self.change_tab)
+        self.bar.setTabsClosable(True)
 
-        # Add the tab widget to the vertical layout
-        vertical_layout.addWidget(self.tab_widget)
-
-    def add_new_tab(self):
-        # Create a new tab widget
-        tab = QWidget()
-
-        # Create the horizontal layout for the tab widget
-        layout = QHBoxLayout(tab)
-
+    def tabSetup(self):
         # Create the graphics view and add it to the layout
-        graphics_view = ZoomView(tab)
+        graphics_view = ZoomView(self.window)
 
         graphics_view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         graphics_view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         graphics_view.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
 
-        layout.addWidget(graphics_view)
+        self.window.layout().addWidget(graphics_view)
 
         # Create the table widget and add it to the layout
-        table_widget = QTableWidget(tab)
+        table_widget = QTableWidget(self.window)
         table_widget.horizontalHeader().setStretchLastSection(True)
         table_widget.setAlternatingRowColors(True)
         table_widget.setShowGrid(True)
 
-        layout.addWidget(table_widget)
+        self.window.layout().addWidget(table_widget)
 
         toolBarLayout = QVBoxLayout()
-        layout.addLayout(toolBarLayout)
+        self.window.layout().addLayout(toolBarLayout)
        
         # Add the "Add File" button to the tab
-        add_file_button = QPushButton("Add File", tab)
+        add_file_button = QPushButton("Add File", self.window)
         add_file_button.clicked.connect(lambda: self.add_file_to_tab(graphics_view))
         toolBarLayout.addWidget(add_file_button)
 
         # Add the "Extract" button to the tab
-        extract_table_button = QPushButton("Extract", tab)
+        extract_table_button = QPushButton("Extract", self.window)
         extract_table_button.clicked.connect(lambda: self.preExtract(graphics_view, table_widget))
         toolBarLayout.addWidget(extract_table_button)
 
         # Add the "Extract" button to the tab
-        export_table_button = QPushButton("Export", tab)
+        export_table_button = QPushButton("Export", self.window)
         export_table_button.clicked.connect(lambda: self.export(table_widget))
         toolBarLayout.addWidget(export_table_button)
 
-        # Add the tab to the tab widget
-        idx = self.tab_widget.count() - 1
-        self.tab_widget.insertTab(idx, tab, "Tab {}".format(self.tab_widget.count()))
-        self.tab_widget.setCurrentIndex(idx)
+    def remove_tab(self, index):
+        layout = self.window.layout()
 
+        self.bar.removeTab(index)
+        if self.bar.count() == 1:
+            for i in reversed(range(layout.count())):
+                w = layout.itemAt(i).widget()
+                layout.removeWidget(w)
+                w.setParent(None)
+
+    def add_new_tab(self):
+        if self.bar.count() == 1:
+            self.tabSetup()
+        # Add the tab to the tab widget
+        idx = self.bar.count() - 1
+        self.bar.insertTab(idx, "Tab {}".format(self.bar.count()))
+        self.bar.setCurrentIndex(idx)
+
+    def change_tab(self, index):
+        return
     
     def add_file_to_tab(self, graphics_view):
 
@@ -112,16 +120,16 @@ class MainWindow(QMainWindow):
                 graphics_view.fitInView(scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
                 file.close()
 
-                idx = self.tab_widget.currentIndex()
+                idx = self.bar.currentIndex()
                 file_name = os.path.basename(file_path).split('/')[-1]
-                self.tab_widget.setTabText(idx, file_name)
+                self.bar.setTabText(idx, file_name)
 
                 self.file_paths[file_name] = file_path
 
     
     def preExtract(self, graphics_view, table_widget):
-        idx = self.tab_widget.currentIndex()
-        file_name = self.tab_widget.tabText(idx)
+        idx = self.bar.currentIndex()
+        file_name = self.bar.tabText(idx)
         
         if file_name not in self.file_paths:
                 return
@@ -149,7 +157,7 @@ class MainWindow(QMainWindow):
 
     def export(self, table_widget):
 
-        file_path, _ = QFileDialog.getSaveFileName(self, "Choose Save Location", filter="CSV FILES(*.csv)")
+        file_path, _ = QFileDialog.getSaveFileName(self, "Choose Save Location", filter="CSV Files(*.csv)")
 
         if not file_path: 
             return
